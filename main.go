a1f4c4e71e68479c1445b09e89b908e23f675ff8
mjc2001 <mjchapman2001@gmail.com>
2022-02-08 12:50:32 +0000

    Basic html and server

--- main.go
+++ main.go
@@ -2,81 +2,34 @@ package main
 
 import (
 	"fmt"
-	"os"
-	//"strings"
+	"log"
+	"net/http"
 
-	"ascii-art/Functions"
+	"ascii-art-web/Functions"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+        fmt.Fprintf(w, "ParseForm() err: %v", err)
+        return
+    }
+
+	text := r.FormValue("input")
+	banner := r.FormValue("banner")
+
+	path := "Banners/"+banner+".txt"
+	chars := functions.FileInit(path)
+
+	result := functions.Transform(text, chars)
+	fmt.Fprintf(w, result)
+}
+
 func main() {
-	args := os.Args[1:]
+	fileServer := http.FileServer(http.Dir("./Static"))
+	http.Handle("/", fileServer)
+	http.Handle("/ascii-art", formHandler)
 
-	 // ascii-art
-	if len(args) == 1 {
-		word := args[0]
-		chars := functions.FileInit("Banners/standard.txt")
-
-		result := functions.Transform(word, chars)
-
-		fmt.Print(result)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
 	}
-	
-
-	/* // fs
-	message := "Usage: go run . [STRING] [BANNER]\n\nEX: go run . something standard"
-
-	if len(args) == 2 {
-		word := args[0]
-		banner := args[1]
-
-		chars, err := CheckBanner(banner)
-		if err != nil {
-			fmt.Println(message)
-			panic(err)
-		}
-
-		result := functions.Transform(word, chars)
-
-		fmt.Print(result)
-	}
-	*/
-
-	/* // options
-	if len(args) == 3 {
-		word := args[0]
-		banner := args[1]
-		optionInput := args[2]
-
-		chars, err := functions.CheckBanner(banner)
-		if err != nil {
-			panic(err)
-		}
-		
-		message := "Usage: go run . [STRING] [BANNER] [OPTION]\n\nEX: go run . something standard --output=<fileName.txt>\nEX: go run . something standard --align=right"
-
-		userOption, err := functions.CheckOption(optionInput)
-		if err != nil {
-			fmt.Println(message)
-			panic(err)
-		}
-
-		switch userOption {
-		case "output":
-			fileName := strings.TrimPrefix(optionInput, "--output=")
-			
-			result := functions.Transform(word, chars)
-
-			os.WriteFile(fileName, []byte(result), 0666)
-		case "align":
-			alignType := strings.TrimPrefix(optionInput, "--align=")
-			result := functions.Transform(word, chars)
-
-			functions.AlignText(result, alignType)
-		case "color":
-		case "reverse":
-		default:
-			fmt.Println(message)
-		}
-	}
-	*/
 }
