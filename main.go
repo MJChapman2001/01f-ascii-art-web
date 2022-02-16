29f6e6450ee05df7b35bb98d8e1ec7d313b15669
mjc2001 <mjchapman2001@gmail.com>
2022-02-16 11:57:48 +0000

    Adjusted to a single page site

--- main.go
+++ main.go
@@ -2,41 +2,53 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 
 	"ascii-art-web/Functions"
 )
 
+type LargeText struct {
+	Ltext string
+}
+
 func formHandler(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-        fmt.Fprintf(w, "ParseForm() err: %v", err)
-        return
-    }
+	tmpl := template.Must(template.ParseFiles("./Templates/index.html"))
 
-	text := r.FormValue("input")
-	banner := r.FormValue("banner")
-
-	path := "Banners/"+banner+".txt"
-	chars := functions.FileInit(path)
-	result := ""
-
-	for i, j:= 0, 0; i < len(text); i++ {
-		if text[i] == '\r'{
-			result += functions.Transform(text[j:i], chars)
-			j = i + 2
-		} else if i == len(text)-1 {
-			result += functions.Transform(text[j:], chars)
+	switch r.Method {
+	case "GET":
+		data := LargeText{Ltext: "Enter some text",}
+		tmpl.Execute(w, data)
+	case "POST":
+		result := ""
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
 		}
+	
+		text := r.FormValue("input")
+		banner := r.FormValue("banner")
+	
+		path := "Banners/"+banner+".txt"
+		chars := functions.FileInit(path)
+	
+		for i, j:= 0, 0; i < len(text); i++ {
+			if text[i] == '\r'{
+				result += functions.Transform(text[j:i], chars)
+				j = i + 2
+			} else if i == len(text)-1 {
+				result += functions.Transform(text[j:], chars)
+			}
+		}
+		
+		data := LargeText{Ltext: result,}
+		tmpl.Execute(w, data)
 	}
-
-	fmt.Fprintf(w, result)
 }
 
 func main() {
-	fileServer := http.FileServer(http.Dir("./Static"))
-	http.Handle("/", fileServer)
-	http.HandleFunc("/ascii-art", formHandler)
+	http.HandleFunc("/", formHandler)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
