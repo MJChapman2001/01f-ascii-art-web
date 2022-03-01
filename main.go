09c58a1fe9533f3616ea2e83fde47e24bc03175a
Kai-Cheok <kai.cheok@pracedo.com>
2022-03-01 13:22:52 +0000

    read me and basic css

--- main.go
+++ main.go
@@ -1,24 +1,22 @@
 package main
 
 import (
+	functions "ascii-art-web/Functions"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-
-	"ascii-art-web/Functions"
 )
 
 type LargeText struct {
-	Input string
+	Input  string
 	Banner string
-	Ltext string
+	Ltext  string
 }
 
 var Tmpl = template.Must(template.ParseGlob("Templates/*.html"))
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
-
 	if r.URL.Path != "/" {
 		http.Error(w, "404 Status not found", http.StatusNotFound)
 		return
@@ -40,7 +38,7 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "ParseForm() err: %v", err)
 			return
 		}
-	
+
 		text := r.FormValue("input")
 		banner := r.FormValue("banners")
 
@@ -49,18 +47,18 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		path := "Banners/"+banner+".txt"
+		path := "Banners/" + banner + ".txt"
 		chars := functions.FileInit(path)
-	
-		for i, j:= 0, 0; i < len(text); i++ {
-			if text[i] == '\r'{
+
+		for i, j := 0, 0; i < len(text); i++ {
+			if text[i] == '\r' {
 				result += functions.Transform(text[j:i], chars)
 				j = i + 2
 			} else if i == len(text)-1 {
 				result += functions.Transform(text[j:], chars)
 			}
 		}
-		
+
 		var data LargeText
 		data.Input = text
 		data.Banner = banner
@@ -76,8 +74,10 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	http.Handle("/static/",
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", formHandler)
-
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
